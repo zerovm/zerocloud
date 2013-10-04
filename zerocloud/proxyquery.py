8890b212515b19de4862e4301390591b08200f27
Constantine Peresypkin <constantine@litestack.com>
2013-10-05 01:54:09 +0300

    cdr label added in to log

--- zerocloud/proxyquery.py
+++ zerocloud/proxyquery.py
@@ -1410,10 +1410,10 @@ class ClusterController(ObjectController):
                                              connection.nexe_headers['x-nexe-cdr-line'],
                                              connection.nexe_headers['x-nexe-status'])
             request.cdr_log.append(body)
-            self.app.logger.info('%s %s (%s) [%s]' % (txn_id,
-                                                      connection.nexe_headers['x-nexe-system'],
-                                                      connection.nexe_headers['x-nexe-cdr-line'],
-                                                      connection.nexe_headers['x-nexe-status']))
+            self.app.logger.info('zerovm-cdr %s %s (%s) [%s]' % (txn_id,
+                                                                 connection.nexe_headers['x-nexe-system'],
+                                                                 connection.nexe_headers['x-nexe-cdr-line'],
+                                                                 connection.nexe_headers['x-nexe-status']))
         else:
             body = ''.join(request.cdr_log)
             append_req = Request.blank('/%s/%s/%s/%s' % (self.app.version,
