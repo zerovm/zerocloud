zerovm/zerocloud

commit c776351f1461f973bda83cb8f919c982b3fbf07e

better pass response headers to user
this patch will pass as many headers as possible from the output of user script/application to the appropriate channels
it is mainly useful for channels of `message/cgi` or `message/http` type
for these channels all their output headers and their status will be passed on to the final reponse the user gets
of course it means that `message/cgi` or `message/http` channel should have no `path` defined
caveats:
- if you define clustered job and have multiple output channels without `path` their bodies will be concatenated as usual, but their headers will overwrite one another in an undefined way, beware!
- if you defined multiple `message/cgi` or `message/http` channels without path the results will be even stranger than that, beware!

zerocloud/proxyquery.py
@@ -36,7 +36,7 @@ from swift.common.constraints import check_utf8, MAX_FILE_SIZE, MAX_HEADER_SIZE,
 from swift.common.swob import Request, Response, HTTPNotFound, \
     HTTPPreconditionFailed, HTTPRequestTimeout, HTTPRequestEntityTooLarge, \
     HTTPBadRequest, HTTPUnprocessableEntity, HTTPServiceUnavailable, \
-    HTTPClientDisconnect, wsgify, HTTPNotImplemented
+    HTTPClientDisconnect, wsgify, HTTPNotImplemented, HeaderKeyDict
 from zerocloud.common import ACCESS_READABLE, ACCESS_CDR, \
     CLUSTER_CONFIG_FILENAME, NODE_CONFIG_FILENAME, TAR_MIMES, \
     POST_TEXT_OBJECT_SYSTEM_MAP, POST_TEXT_ACCOUNT_SYSTEM_MAP, \
@@ -55,6 +55,8 @@ try:
     import simplejson as json
 except ImportError:
     import json
+
+STRIP_PAX_HEADERS = ['mtime']
 
 
 # Monkey patching Request to support content_type property properly
@@ -76,7 +78,7 @@ def _req_content_type_property():
 Request.content_type = _req_content_type_property()
 
 
-def check_headers_metadata(new_req, headers, target_type, req):
+def check_headers_metadata(new_req, headers, target_type, req, add_all=False):
     prefix = 'x-%s-meta-' % target_type.lower()
     meta_count = 0
     meta_size = 0
@@ -86,6 +88,8 @@ def check_headers_metadata(new_req, headers, target_type, req):
                                       key[:MAX_META_NAME_LENGTH],
                                  request=req, content_type='text/plain')
         if not key.lower().startswith(prefix):
+            if add_all and key.lower() not in STRIP_PAX_HEADERS:
+                new_req.headers[key] = value
             continue
         new_req.headers[key] = value
         key = key[len(prefix):]
@@ -789,28 +793,23 @@ class ClusterController(ObjectController):
         req.cdr_log = []
         for conn in conns:
             resp = conn.resp
-            got_nexe_header = False
-            if resp:
-                for key in conn.nexe_headers.keys():
-                    if resp.headers.get(key):
-                        got_nexe_header = True
-                        conn.nexe_headers[key] = resp.headers.get(key)
             if conn.error:
                 conn.nexe_headers['x-nexe-error'] = \
                     conn.error.replace('\n', '')
                 if conn.resp.status_int > final_response.status_int:
                     final_response.status = conn.resp.status
-            else:
-                if is_success(resp.status_int) and not got_nexe_header:
-                    # looks like the middleware is not installed
-                    # or not functioning otherwise we should get something
-                    return HTTPServiceUnavailable(
-                        request=req,
-                        headers=resp.headers,
-                        body='objectquery middleware is not installed '
-                             'or not functioning')
+            merge_headers(final_response.headers, conn.nexe_headers,
+                          resp.headers)
             self._store_accounting_data(req, conn)
-            merge_headers(final_response.headers, conn.nexe_headers)
+            if is_success(resp.status_int) and 'x-nexe-status' not in \
+                    resp.headers:
+                # looks like the middleware is not installed
+                # or not functioning otherwise we should get something
+                return HTTPServiceUnavailable(
+                    request=req,
+                    headers=resp.headers,
+                    body='objectquery middleware is not installed '
+                         'or not functioning')
             if resp and resp.headers.get('x-zerovm-daemon', None):
                 final_response.headers['x-nexe-cached'] = 'true'
             if resp and resp.content_length > 0:
@@ -1081,7 +1080,7 @@ class ClusterController(ObjectController):
                 return HTTPServiceUnavailable(body='Cannot bind name service')
         exec_requests = []
         for node in self.parser.node_list:
-            nexe_headers = {
+            nexe_headers = HeaderKeyDict({
                 'x-nexe-system': node.name,
                 'x-nexe-status': 'ZeroVM did not run',
                 'x-nexe-retcode': 0,
@@ -1089,7 +1088,7 @@ class ClusterController(ObjectController):
                 'x-nexe-validation': 0,
                 'x-nexe-cdr-line': '0.0 0.0 0 0 0 0 0 0 0 0',
                 'x-nexe-policy': ''
-            }
+            })
             path_info = req.path_info
             exec_request = Request.blank(path_info,
                                          environ=req.environ,
@@ -1345,7 +1344,7 @@ class ClusterController(ObjectController):
 
     def process_server_response(self, conn, request, resp):
         conn.resp = resp
-        if resp.status_int >= 300:
+        if not is_success(resp.status_int):
             conn.error = resp.body
             return conn
         if resp.content_length == 0:
@@ -1374,9 +1373,8 @@ class ClusterController(ObjectController):
                     resp.app_iter = app_iter
                     resp.content_length = headers['Content-Length']
                     resp.content_type = headers['Content-Type']
-                    check_headers_metadata(resp, headers, 'object', request)
-                    if headers.get('Status'):
-                        resp.headers['status'] = headers['Status']
+                    check_headers_metadata(resp, headers, 'object', request,
+                                           add_all=True)
                     return conn
                 dest_req = Request.blank(chan.path.path,
                                          environ=request.environ,

test/unit/test_proxyquery.py
@@ -997,8 +997,8 @@ class TestProxyQuery(unittest.TestCase):
         res = req.get_response(prosrv)
         self.assertEqual(res.status_int, 200)
         self.assertEqual(res.headers['content-type'], 'text/html')
-        self.assertNotIn('x-object-meta-key1', res.headers)
-        self.assertNotIn('x-object-meta-key2', res.headers)
+        self.assertEqual(res.headers['x-object-meta-key1'], 'value1')
+        self.assertEqual(res.headers['x-object-meta-key2'], 'value2')
         self.assertEqual(res.body, '<html><body>Test this</body></html>')
         self.check_container_integrity(prosrv, '/v1/a/c', {})
 

zerocloud/common.py
@@ -108,19 +108,17 @@ RE_ILLEGAL = u'([\u0000-\u0008\u000b-\u000c\u000e-\u001f\ufffe-\uffff])' + \
 TIMEOUT_GRACE = 0.5
 
 
-def merge_headers(current, new):
-    if hasattr(new, 'keys'):
-        for key in new.keys():
-            if not current[key.lower()]:
-                current[key.lower()] = str(new[key])
-            else:
-                current[key.lower()] += ',' + str(new[key])
-    else:
-        for key, value in new:
-            if not current[key.lower()]:
-                current[key.lower()] = str(value)
-            else:
-                current[key.lower()] += ',' + str(value)
+def merge_headers(final, mergeable, new):
+    key_list = mergeable.keys()
+    for key in key_list:
+        mergeable[key] = new.get(key, mergeable[key])
+        if not final.get(key):
+            final[key] = str(mergeable[key])
+        else:
+            final[key] += ',' + str(mergeable[key])
+    for key in new.keys():
+        if key not in key_list:
+            final[key] = new[key]
 
 
 def has_control_chars(line):

commit 2edf79c86b875e4ca08b90e4d6408b115de2fa9f

fix x-nexe- headers bug
fix bug introduced in c776351f1461f973bda83cb8f919c982b3fbf07e
that allows user to overwrite `X-Nexe-` headers in response

zerocloud/proxyquery.py
@@ -88,7 +88,8 @@ def check_headers_metadata(new_req, headers, target_type, req, add_all=False):
                                       key[:MAX_META_NAME_LENGTH],
                                  request=req, content_type='text/plain')
         if not key.lower().startswith(prefix):
-            if add_all and key.lower() not in STRIP_PAX_HEADERS:
+            if add_all and key.lower() not in STRIP_PAX_HEADERS and not \
+                    key.lower().startswith('x-nexe-'):
                 new_req.headers[key] = value
             continue
         new_req.headers[key] = value

test/unit/test_proxyquery.py
@@ -3454,3 +3454,44 @@ class TestProxyQuery(unittest.TestCase):
             req.headers['x-zerovm-execute'] = 'open/1.0'
             res = req.get_response(prosrv)
             self.assertEqual(res.status_int, 501)
+
+    def test_setting_nexe_headers(self):
+        self.setup_QUERY()
+        prolis = _test_sockets[0]
+        prosrv = _test_servers[0]
+        nexe = trim(r'''
+            resp = '\n'.join([
+                'HTTP/1.1 200 OK',
+                'Content-Type: text/html',
+                'X-Nexe-Status: stat',
+                'X-Nexe-Retcode: 42',
+                '', ''
+                ])
+            out = '<html><body>Test this</body></html>'
+            return resp + out
+            ''')
+        self.create_object(prolis, '/v1/a/c/exe2', nexe)
+        conf = [
+            {
+                'name': 'http',
+                'exec': {'path': 'swift://a/c/exe2'},
+                'file_list': [
+                    {
+                        'device': 'stdout',
+                        'content_type': 'message/http'
+                    }
+                ]
+            }
+        ]
+        conf = json.dumps(conf)
+        req = self.zerovm_request()
+        req.body = conf
+        res = req.get_response(prosrv)
+        self.assertEqual(res.status_int, 200)
+        self.assertEqual(res.headers['content-type'], 'text/html')
+        self.assertEqual(res.body, '<html><body>Test this</body></html>')
+        # we should get actual x-nexe- headers
+        # and not the ones user application sets in its output
+        self.assertEqual(res.headers['x-nexe-status'], 'ok.')
+        self.assertEqual(res.headers['x-nexe-retcode'], '0')
+        self.check_container_integrity(prosrv, '/v1/a/c', {})